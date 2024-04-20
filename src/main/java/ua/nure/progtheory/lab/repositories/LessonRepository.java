dda61f40a7c946146af6e22436d5a8f05193ca3b
vlaadkshl <koshel.vladyslav@gmail.com>
2024-04-20 18:58:19 +0200

    added AppController

--- src/main/java/ua/nure/progtheory/lab/repositories/LessonRepository.java
+++ src/main/java/ua/nure/progtheory/lab/repositories/LessonRepository.java
@@ -3,5 +3,12 @@ package ua.nure.progtheory.lab.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.nure.progtheory.lab.data.Lesson;
 
+import java.util.List;
+
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
+    List<Lesson> findByGroupId(Long groupId);
+
+    List<Lesson> findByTeacherId(Long teacherId);
+
+    List<Lesson> findBySubjectId(Long subjectId);
 }
