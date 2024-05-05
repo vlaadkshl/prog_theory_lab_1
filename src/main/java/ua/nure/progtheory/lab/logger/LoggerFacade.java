0d073d16d3a04e8e674976973d5d4a772edccdc0
vlaadkshl <koshel.vladyslav@gmail.com>
2024-05-05 23:09:42 +0200

    add LoggerFacade, which hides logic of creating event and sending it to WebRequestEventPublisher

---
+++ src/main/java/ua/nure/progtheory/lab/logger/LoggerFacade.java
@@ -0,0 +1,19 @@
+package ua.nure.progtheory.lab.logger;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class LoggerFacade {
+    private final WebRequestEventPublisher webRequestEventPublisher = new WebRequestEventPublisher();
+
+    public LoggerFacade() {
+        webRequestEventPublisher.addObserver(FileLogger.getInstance());
+    }
+
+    public void logRequest(String requestType, String requestDetails) {
+        RequestEvent requestEvent = new RequestEvent(LocalDateTime.now(), requestType, requestDetails);
+        webRequestEventPublisher.notifyObservers(requestEvent);
+    }
+}
