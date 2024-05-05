6ba9d8622baea4fdaf22fdfabd24f78fd0103466
vlaadkshl <koshel.vladyslav@gmail.com>
2024-05-05 23:09:05 +0200

    add WebRequestEventPublisher for possible multiple loggers (using Observer pattern)

---
+++ src/main/java/ua/nure/progtheory/lab/logger/WebRequestEventPublisher.java
@@ -0,0 +1,22 @@
+package ua.nure.progtheory.lab.logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WebRequestEventPublisher {
+    private final List<WebLogger> listeners = new ArrayList<>();
+
+    public void addObserver(WebLogger observer) {
+        listeners.add(observer);
+    }
+
+    public void removeObserver(WebLogger observer) {
+        listeners.remove(observer);
+    }
+
+    public void notifyObservers(RequestEvent requestEvent) {
+        for (WebLogger listener : listeners) {
+            listener.log(requestEvent.timeOfRequest(), requestEvent.requestType(), requestEvent.requestDetails());
+        }
+    }
+}
