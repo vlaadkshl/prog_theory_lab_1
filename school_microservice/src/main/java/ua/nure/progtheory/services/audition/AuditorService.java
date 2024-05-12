4b6655bfb3a1a1e90a66bf203d2bb8db8a7fbf37
vlaadkshl <koshel.vladyslav@gmail.com>
2024-05-12 22:49:21 +0200

    added audition for updating, creation and deleting in school-microservice

--- school_microservice/src/main/java/ua/nure/progtheory/services/audition/AuditorService.java
+++ school_microservice/src/main/java/ua/nure/progtheory/services/audition/AuditorService.java
@@ -22,18 +22,38 @@ public class AuditorService {
     private record AuditData(String tableName, String auditData, Timestamp timestamp) {
     }
 
-    public void auditReading(Object entity, String entityName) {
+    public void auditReading(String data, String entityName) {
+        audit(data, entityName, "read_data");
+    }
+
+    public void auditUpdating(Object data, String entityName) {
         try {
-            auditReading(objectMapper.writeValueAsString(entity), entityName);
+            audit(objectMapper.writeValueAsString(data), entityName, "update_data");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void auditReading(String data, String entityName) {
+    public void auditCreation(Object data, String entityName) {
+        try {
+            audit(objectMapper.writeValueAsString(data), entityName, "create_data");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void auditDeleting(Object data, String entityName) {
+        try {
+            audit(objectMapper.writeValueAsString(data), entityName, "delete_data");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void audit(String data, String entityName, String topicName) {
         try {
             AuditData auditData = new AuditData(entityName, data, new Timestamp(System.currentTimeMillis()));
-            sendMessage("read_data", objectMapper.writeValueAsString(auditData));
+            sendMessage(topicName, objectMapper.writeValueAsString(auditData));
         } catch (Exception e) {
             e.printStackTrace();
         }
